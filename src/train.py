9d912a723847912b1209683d6788da4dcd2a0be0
Steven Nguyen <steventn96@gmail.com>
2024-03-02 12:30:26 -0800

    created custom data pipeline before implementation of CNN

--- src/train.py
+++ src/train.py
@@ -1,142 +1,81 @@
-import os
-import pandas as pd
-import torch
-import torch.nn as nn
-import torchvision.models as models
-import torch.optim as optim
-
-from sklearn.preprocessing import LabelEncoder
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, datasets
+import os, torch, shutil, numpy as np, pandas as pd
+from glob import glob
 from PIL import Image
+from torch.utils.data import random_split, Dataset, DataLoader
+from torchvision import transforms as T
+
+torch.manual_seed(2024)
 
 
 class CustomDataset(Dataset):
-    def __init__(self, labels_df, root_dir, transform=None):
-        self.labels_df = labels_df
-        self.root_dir = root_dir
-        self.transform = transform
+
+    def __init__(self, root, transformations=None):
+        self.transformations, self.root = transformations, root
+        self.img_paths = sorted(glob(os.path.join(root, "e-commerce", "images", "*")))
+        data_path = os.path.join(root, "styles.csv")
+        print(f"Attempting to read data from: {data_path}")
+        data = pd.read_csv(data_path)
+        ids = list(data["id"])
+        label = list(data["subCategory"])
+
+        self.ids, self.label = [], []
+        self.cls_names, self.cls_counts, count, data_count = {}, {}, 0, 0
+        for idx, (id, class_name) in enumerate(zip(ids, label)):
+            self.ids.append(id)
+            self.label.append(class_name)
+            if class_name not in self.cls_names:
+                self.cls_names[class_name] = count
+                self.cls_counts[class_name] = 1
+                count += 1
+            else:
+                self.cls_counts[class_name] += 1
 
     def __len__(self):
-        return len(self.labels_df)
+        return len(self.img_paths)
 
     def __getitem__(self, idx):
-        img_name = str(self.labels_df.iloc[idx, 0])
-        img_path = os.path.join(script_dir, '..', 'data', 'e-commerce', 'images', str(img_name) + '.jpg')
-        image = Image.open(img_path)
+        img = Image.open(os.path.join(self.root, "e-commerce", "images", f"{self.ids[idx]}.jpg")).convert("RGB")
+        true_label = self.cls_names[self.label[idx]]
 
-        # Extract the label columns
-        gender = self.labels_df.iloc[idx, 1]
-        master_category = self.labels_df.iloc[idx, 2]
-        sub_category = self.labels_df.iloc[idx, 3]
-        article_type = self.labels_df.iloc[idx, 4]
-        base_color = self.labels_df.iloc[idx, 5]
-        season = self.labels_df.iloc[idx, 6]
-        year = self.labels_df.iloc[idx, 7]
-        usage = self.labels_df.iloc[idx, 8]
-        display_name = self.labels_df.iloc[idx, 9]
+        if self.transformations is not None: img = self.transformations(img)
 
-        label = {
-            'gender': gender,
-            'master_category': master_category,
-            'sub_category': sub_category,
-            'article_type': article_type,
-            'base_color': base_color,
-            'season': season,
-            'year': year,
-            'usage': usage,
-            'display_name': display_name
-        }
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
+        return img, true_label
 
 
-def collate_fn(batch):
-    images, labels = zip(*batch)
+def get_dls(root, transformations, bs, split=[0.9, 0.05, 0.05], ns=4):
+    ds = CustomDataset(root=root, transformations=transformations)
 
-    # No need to apply ToTensor() here as images are already transformed in CustomDataset
-    # Resize images to a common size if needed
-    images = [data_transform(image) for image in images]
+    total_len = len(ds)
+    tr_len = int(total_len * split[0])
+    vl_len = int(total_len * split[1])
+    ts_len = total_len - (tr_len + vl_len)
 
-    # Stack images and handle varying channel sizes
-    max_channels = max(image.shape[0] for image in images)
-    stacked_images = torch.stack(
-        [torch.cat([image, torch.zeros(max_channels - image.shape[0], *image.shape[1:])]) for image in images])
+    tr_ds, vl_ds, ts_ds = random_split(dataset=ds, lengths=[tr_len, vl_len, ts_len])
 
-    return stacked_images, labels
+    tr_dl, val_dl, ts_dl = DataLoader(tr_ds, batch_size=bs, shuffle=True, num_workers=ns), DataLoader(vl_ds,
+                                                                                                      batch_size=bs,
+                                                                                                      shuffle=False,
+                                                                                                      num_workers=ns), DataLoader(
+        ts_ds, batch_size=1, shuffle=False, num_workers=ns)
 
-
-# Load the CSV file containing labels
-script_dir = os.path.dirname(os.path.realpath(__file__))
-csv_file_path = os.path.join(script_dir, '..', 'data', 'styles.csv')
-labels_df = pd.read_csv(csv_file_path)
-
-# Create an instance of your custom dataset
-data_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-])
-
-data_folder = os.path.join('data', 'e-commerce', 'images')
-custom_dataset = CustomDataset(labels_df, os.path.join(script_dir, data_folder), transform=data_transform)
-
-# Create data loader
-train_loader = DataLoader(custom_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn)
+    return tr_dl, val_dl, ts_dl, ds.cls_names
 
 
-class CustomModel(nn.Module):
-    def __init__(self, num_classes):
-        super(CustomModel, self).__init__()
-        self.base_model = models.resnet18(pretrained=True)
-        self.base_model.fc = nn.Linear(self.base_model.fc.in_features, num_classes)
+def main():
+    curr_dir = os.path.dirname(os.path.realpath(__file__))
+    root = os.path.abspath(os.path.join(curr_dir, ".."))
+    data_dir = os.path.join(root, "data")
 
-    def forward(self, x):
-        return self.base_model(x)
+    mean, std, im_size = [0.485, 0.456, 0.406], [0.229, 0.224, 0.225], 224
+    tfs = T.Compose([T.Resize((im_size, im_size)), T.ToTensor(), T.Normalize(mean=mean, std=std)])
+
+    tr_dl, val_dl, ts_dl, classes = get_dls(root=os.path.join(data_dir), transformations=tfs, bs=32)
+
+    print(f"Train DataLoader length: {len(tr_dl)}")
+    print(f"Validation DataLoader length: {len(val_dl)}")
+    print(f"Test DataLoader length: {len(ts_dl)}")
+    print(f"Classes: {classes}")
 
 
-# Create an instance of your model
-num_classes = len(labels_df['articleType'].unique())
-model = CustomModel(num_classes)
-
-# Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Instantiate label encoder
-label_encoder = LabelEncoder()
-
-# Label encode the target variable outside the training loop
-labels_df['encoded_article_type'] = label_encoder.fit_transform(labels_df['articleType'])
-
-# Create an instance of your model
-num_classes = len(label_encoder.classes_)
-model = CustomModel(num_classes)
-
-# Move the model to GPU if available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.to(device)
-
-# Training loop
-num_epochs = 10
-for epoch in range(num_epochs):
-    total_loss = 0.0
-    for inputs, labels in train_loader:
-        inputs, target = inputs.to(device), labels['encoded_article_type'].to(device)
-
-        optimizer.zero_grad()
-        outputs = model(inputs)
-
-        loss = criterion(outputs, target)
-        loss.backward()
-        optimizer.step()
-
-        total_loss += loss.item()
-
-    average_loss = total_loss / len(train_loader)
-    print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {average_loss:.4f}')
-
-# Save the trained model
-torch.save(model.state_dict(), 'models/your_model.pth')
+if __name__ == "__main__":
+    main()
