3506ea07ff205179af8f19b2443414bb0bd42ebd
Steven Nguyen <steventn96@gmail.com>
2024-03-21 23:37:17 -0700

    updated app to include relative path

--- style_recommend_backend/app.py
+++ style_recommend_backend/app.py
@@ -1,6 +1,11 @@
-import os
+import os, sys
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+sys.path.append(parent_dir)
+
 from src.interface import get_recommendations
 from src.color_handler import get_color_recommendations
 
